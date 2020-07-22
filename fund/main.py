1074f63fb1aa258522f9419c6c7e9095a843019d
Oliver Xu <273601727@qq.com>
2020-07-22 19:08:31 +0800

    更新IBM配置

--- fund/main.py
+++ fund/main.py
@@ -7,14 +7,14 @@ import funddata
 import recommend
 
 st.title('基金推荐和数据分析工具')
-
+st.info('[贡献本项目](https://github.com/olixu/Fund)')
 # 按钮：推荐最新数据，先查看database目录下是否有历史数据库，没有的话输出：请爬取历史数据
 st.header("爬取最新数据")
 if st.button('爬取最新数据'):
     if funddata.check_databases():
-        st.write("正在生成推荐")
+        st.info("正在生成推荐")
         os.system('python recommend.py')
-        st.write("推送已发送")
+        st.success("推送已发送")
     else:
         st.write("没有历史数据库，请爬取历史数据")
 
@@ -22,10 +22,11 @@ if st.button('爬取最新数据'):
 st.header("爬取历史数据")
 if st.button('爬取历史数据'):
     if funddata.check_databases():
-        st.write("有历史数据，无需重新爬取！")
+        st.info("有历史数据，无需重新爬取！")
     else:
-        st.write("没有历史数据，需要重新爬取！")
+        st.info("没有历史数据，需要重新爬取！")
         os.system('python funddata.py -m past')
+
 
 # 强制重新爬取基金历史数据
 st.header("强制重新爬取基金历史数据")
--- fund/recommend.py
+++ fund/recommend.py
@@ -25,67 +25,70 @@ c1 = conn1.cursor()
 conn2 = sqlite3.connect("../database/fundhistory.db")
 c2 = conn2.cursor()
 
-codes = []
-data = []
-tables = c2.execute("select tbl_name from sqlite_master")
-date_p = str(datetime.datetime.now().date())
+def recom():
+    codes = []
+    data = []
+    tables = c2.execute("select tbl_name from sqlite_master")
+    date_p = str(datetime.datetime.now().date())
+    # 获取所有的
+    for i in tables:
+        codes.append(i[0])
+    # 删除code中重复的部分
+    codes = sorted(set(codes), key = codes.index)
 
-for i in tables:
-    codes.append(i[0])
+    for code in codes:
+        # 先删除所有的历史的最新数据，方便验证
+        #outcome = c2.execute("select * from '" + code + "' where date='{}'".format('2020-07-20'))
+        outcome = c2.execute("select * from '" + code + "' where date='{}'".format(date_p))
+        for _ in outcome:
+            x = list(_)
+            x.insert(0,code)
+            data.append(x)
 
-# 删除code中重复的部分
-codes = sorted(set(codes), key = codes.index)
+    df = pd.DataFrame(data, columns=['代码', '时期', '净值', '累计净值', '日涨跌幅', '最近一周涨跌幅', '最近一月涨跌幅', '最近三周涨跌幅', '最近六涨跌幅', '最近一年涨跌幅', '最近两年涨跌幅', '最近三年涨跌幅', '今年以来涨跌幅', '成立以来涨跌幅'])
+    # 将数据转化成浮点数
+    df[['净值', '累计净值', '日涨跌幅', '最近一周涨跌幅', '最近一月涨跌幅', '最近三周涨跌幅', '最近六涨跌幅', '最近一年涨跌幅', '最近两年涨跌幅', '最近三年涨跌幅', '今年以来涨跌幅', '成立以来涨跌幅']] = df[['净值', '累计净值', '日涨跌幅', '最近一周涨跌幅', '最近一月涨跌幅', '最近三周涨跌幅', '最近六涨跌幅', '最近一年涨跌幅', '最近两年涨跌幅', '最近三年涨跌幅', '今年以来涨跌幅', '成立以来涨跌幅']].apply(pd.to_numeric, errors='ignore')
 
-for code in codes:
-    # 先删除所有的历史的最新数据，方便验证
-    #outcome = c2.execute("select * from '" + code + "' where date='{}'".format('2020-07-20'))
-    outcome = c2.execute("select * from '" + code + "' where date='{}'".format(date_p))
-    for _ in outcome:
-        x = list(_)
-        x.insert(0,code)
-        data.append(x)
+    # 设置一定的策略来挑选合适的基金
+    # 选择最近一年涨幅大于50且小于1000的
+    record = df[(df['最近一年涨跌幅']>50) & (df['最近一年涨跌幅']<1000)]
+    record1 = record[record['最近一周涨跌幅']<-2.0]
+    print(record1)
+    record2 = record[record['最近一周涨跌幅']>1.0]
+    print(record2)
 
-df = pd.DataFrame(data, columns=['代码', '时期', '净值', '累计净值', '日涨跌幅', '最近一周涨跌幅', '最近一月涨跌幅', '最近三周涨跌幅', '最近六涨跌幅', '最近一年涨跌幅', '最近两年涨跌幅', '最近三年涨跌幅', '今年以来涨跌幅', '成立以来涨跌幅'])
-# 将数据转化成浮点数
-df[['净值', '累计净值', '日涨跌幅', '最近一周涨跌幅', '最近一月涨跌幅', '最近三周涨跌幅', '最近六涨跌幅', '最近一年涨跌幅', '最近两年涨跌幅', '最近三年涨跌幅', '今年以来涨跌幅', '成立以来涨跌幅']] = df[['净值', '累计净值', '日涨跌幅', '最近一周涨跌幅', '最近一月涨跌幅', '最近三周涨跌幅', '最近六涨跌幅', '最近一年涨跌幅', '最近两年涨跌幅', '最近三年涨跌幅', '今年以来涨跌幅', '成立以来涨跌幅']].apply(pd.to_numeric, errors='ignore')
+    # 关闭数据库连接
+    conn1.commit()
+    conn1.close()
+    conn2.commit()
+    conn2.close()
 
-# 设置一定的策略来挑选合适的基金
-# 选择最近一年涨幅大于50且小于1000的
-record = df[(df['最近一年涨跌幅']>50) & (df['最近一年涨跌幅']<1000)]
-record1 = record[record['最近一周涨跌幅']<-2.0]
-print(record1)
-record2 = record[record['最近一周涨跌幅']>1.0]
-print(record2)
+    # 设置推送
+    buy_codes = record1['代码']
+    sell_codes = record2['代码']
 
-# 关闭数据库连接
-conn1.commit()
-conn1.close()
-conn2.commit()
-conn2.close()
+    buy = '''
+    ## 建议申购的基金
 
-# 设置推送
-buy_codes = record1['代码']
-sell_codes = record2['代码']
+    '''
 
-buy = '''
-## 建议申购的基金
+    sell = '''
+    ## 建议赎回的基金
 
-'''
+    '''
+    for i in buy_codes:
+        buy = buy + i + '\n\n'
 
-sell = '''
-## 建议赎回的基金
+    for i in sell_codes:
+        sell = sell + i + '\n\n'
 
-'''
-for i in buy_codes:
-    buy = buy + i + '\n\n'
+    data1 = {
+        'text': '今日基金推荐',
+        'desp': buy + '\n\n' + sell
+    }
 
-for i in sell_codes:
-    sell = sell + i + '\n\n'
+    response = requests.post(url="https://sc.ftqq.com/SCU99784Te024d0b9ba13f4e59deaf0fbc30ed1df5ed0dbc5548c4.send", data=data1)
+    print(response)
 
-data1 = {
-    'text': '今日基金推荐',
-    'desp': buy + '\n\n' + sell
-}
-
-response = requests.post(url="https://sc.ftqq.com/SCU99784Te024d0b9ba13f4e59deaf0fbc30ed1df5ed0dbc5548c4.send", data=data1)
-print(response)
+if __name__=="__main__":
+    recom()
